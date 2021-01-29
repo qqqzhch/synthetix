cfc8a78b49711f845f801f171fefec8a60421fcd
songliling <847376582@qq.com>
2021-01-29 11:25:11 +0800

    Add query lock amount method

--- contracts/SynthetixProxy.sol
+++ contracts/SynthetixProxy.sol
@@ -142,127 +142,6 @@ contract ERC20 is ERC20Basic {
     );
 }
 
-// File: openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol
-
-/**
- * @title Standard ERC20 token
- *
- * @dev Implementation of the basic standard token.
- * https://github.com/ethereum/EIPs/issues/20
- * Based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
- */
-contract StandardToken is ERC20, BasicToken {
-
-    mapping (address => mapping (address => uint256)) internal allowed;
-
-
-    /**
-     * @dev Transfer tokens from one address to another
-     * @param _from address The address which you want to send tokens from
-     * @param _to address The address which you want to transfer to
-     * @param _value uint256 the amount of tokens to be transferred
-     */
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _value
-    )
-    public
-    returns (bool)
-    {
-        require(_value <= balances[_from]);
-        require(_value <= allowed[_from][msg.sender]);
-        require(_to != address(0));
-
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-        emit Transfer(_from, _to, _value);
-        return true;
-    }
-
-    /**
-     * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
-     * Beware that changing an allowance with this method brings the risk that someone may use both the old
-     * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
-     * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     * @param _spender The address which will spend the funds.
-     * @param _value The amount of tokens to be spent.
-     */
-    function approve(address _spender, uint256 _value) public returns (bool) {
-        allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    /**
-     * @dev Function to check the amount of tokens that an owner allowed to a spender.
-     * @param _owner address The address which owns the funds.
-     * @param _spender address The address which will spend the funds.
-     * @return A uint256 specifying the amount of tokens still available for the spender.
-     */
-    function allowance(
-        address _owner,
-        address _spender
-    )
-    public
-    view
-    returns (uint256)
-    {
-        return allowed[_owner][_spender];
-    }
-
-    /**
-     * @dev Increase the amount of tokens that an owner allowed to a spender.
-     * approve should be called when allowed[_spender] == 0. To increment
-     * allowed value is better to use this function to avoid 2 calls (and wait until
-     * the first transaction is mined)
-     * From MonolithDAO Token.sol
-     * @param _spender The address which will spend the funds.
-     * @param _addedValue The amount of tokens to increase the allowance by.
-     */
-    function increaseApproval(
-        address _spender,
-        uint256 _addedValue
-    )
-    public
-    returns (bool)
-    {
-        allowed[msg.sender][_spender] = (
-        allowed[msg.sender][_spender].add(_addedValue));
-        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        return true;
-    }
-
-    /**
-     * @dev Decrease the amount of tokens that an owner allowed to a spender.
-     * approve should be called when allowed[_spender] == 0. To decrement
-     * allowed value is better to use this function to avoid 2 calls (and wait until
-     * the first transaction is mined)
-     * From MonolithDAO Token.sol
-     * @param _spender The address which will spend the funds.
-     * @param _subtractedValue The amount of tokens to decrease the allowance by.
-     */
-    function decreaseApproval(
-        address _spender,
-        uint256 _subtractedValue
-    )
-    public
-    returns (bool)
-    {
-        uint256 oldValue = allowed[msg.sender][_spender];
-        if (_subtractedValue >= oldValue) {
-            allowed[msg.sender][_spender] = 0;
-        } else {
-            allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
-        }
-        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
-        return true;
-    }
-
-}
-
 // File: openzeppelin-solidity/contracts/ownership/Ownable.sol
 
 /**
@@ -331,26 +210,33 @@ contract SynthetixProxy is Ownable {
     using SafeMath for uint256;
     address synth;
     address ERCLamb;
-    mapping(address => uint) public lockInfo;
+    mapping(address => uint) lockInfo;
 
-    function setSynthAddress(address _synth) public onlyOwner {
+    function setSynthAddress(address _synth) external onlyOwner {
         synth = _synth;
     }
 
-    function setERCLambAddress(address _ERCLamb) public onlyOwner {
+    function setERCLambAddress(address _ERCLamb) external onlyOwner {
         ERCLamb = _ERCLamb;
     }
 
-    function lock(uint value) public {
+    function lock(uint value) external {
         require(ERC20(ERCLamb).transferFrom(msg.sender, this, value), "transferFrom error");
-        lockInfo[msg.sender] = lockInfo[msg.sender].add(value);
         ISynContract(synth).mint(msg.sender, value);
+        lockInfo[msg.sender] = lockInfo[msg.sender].add(value);
     }
 
-    function unlock(uint value) public {
+    function unlock(uint value) external {
         require(lockInfo[msg.sender] >= value, "not enough token");
-        lockInfo[msg.sender] = lockInfo[msg.sender].sub(value);
         ISynContract(synth).burn(msg.sender, value);
+        lockInfo[msg.sender] = lockInfo[msg.sender].sub(value);
         ERC20(ERCLamb).transfer(msg.sender, value);
+    }
+
+    /* View */
+
+    function lockAmount(address _address) external view returns (uint) {
+        require(_address != address(0));
+        return lockInfo[_address];
     }
 }
